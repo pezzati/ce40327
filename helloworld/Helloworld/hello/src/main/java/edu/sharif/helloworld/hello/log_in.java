5c25583391353097f3f212ecb10d7945c4d9cea8
pezzati <p.ezzati.ab@gmail.com>
2014-10-27 08:53:50 +0330

    negar testing :D

--- helloworld/Helloworld/hello/src/main/java/edu/sharif/helloworld/hello/log_in.java
+++ helloworld/Helloworld/hello/src/main/java/edu/sharif/helloworld/hello/log_in.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 public class log_in extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
